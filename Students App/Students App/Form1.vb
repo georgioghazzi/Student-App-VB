2eb1bd01f5ec65272b165518a6011b71d216cddb
Georgio Ghazzi <ghazzi94@gmail.com>
2018-05-09 22:37:59 +0300

    Initial commit

---
+++ Students App/Students App/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+
+    Private Sub Close_Click(sender As Object, e As EventArgs) Handles Close.Click
+        Application.Exit()
+    End Sub
+
+
+
+    Private Sub Minimize_Click(sender As Object, e As EventArgs) Handles Minimize.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+
+End Class
